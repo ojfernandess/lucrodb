e3c66818c7d01e2d08d94f798637822916ec71f4
ojfernandess <odairjosfernandess@gmail.com>
2025-01-15 10:19:04 -0300

    Update profitModel.js

--- models/profitModel.js
+++ models/profitModel.js
@@ -1,11 +1,26 @@
 const mongoose = require('mongoose');
 
+// Definir o esquema de lucro
 const profitSchema = new mongoose.Schema({
-  planId: { type: String, required: true, unique: true },
-  profit: { type: Number, required: true },
-  startTime: { type: Number, required: true }
+  userId: {
+    type: String,
+    required: true
+  },
+  planId: {
+    type: String,
+    required: true
+  },
+  profitAmount: {
+    type: Number,
+    required: true
+  },
+  timestamp: {
+    type: Date,
+    default: Date.now
+  }
 });
 
+// Criar o modelo com base no esquema
 const Profit = mongoose.model('Profit', profitSchema);
 
 module.exports = Profit;
