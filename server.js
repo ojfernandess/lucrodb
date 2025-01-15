8c90a8b8ebfa562189cd613faff329e423812a96
ojfernandess <odairjosfernandess@gmail.com>
2025-01-15 09:57:42 -0300

    Update server.js

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+console.log('MONGODB_URI:', process.env.MONGODB_URI);  // Adicione essa linha para verificar
+
 // Conectar ao MongoDB
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
